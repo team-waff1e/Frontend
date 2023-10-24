4b9db3e4ae16a797009daa43b9506e3621926c15
KTG <xorb269@naver.com>
2023-10-25 02:03:55 +0900

    준비 끝

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,7 +1,27 @@
+import { Form, Input, Title, Wrapper } from "../components/signup-form";
+
 export default function Signup() {
   return (
-    <>
-      <h1>Signup</h1>
-    </>
+    <Wrapper>
+      <Title>Signup</Title>
+      <Form>
+        <Input name="email" placeholder="email" type="text" required />
+        <Input name="username" placeholder="username" type="text" required />
+        <Input
+          name="password"
+          placeholder="password"
+          type="password"
+          required
+        />
+        <Input
+          name="passwordConfirm"
+          placeholder="password confirm"
+          type="password"
+          required
+        />
+        <Input name="nickname" placeholder="nickname" type="text" required />
+        <Input type="submit" value="제출" required />
+      </Form>
+    </Wrapper>
   );
 }
