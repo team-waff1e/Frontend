370de877235e81172622a3c344f5cda920aa6bf1
hhheeeeee <libravickyyy@gmail.com>
2023-10-30 10:50:35 +0900

    라우팅 수정, 회원가입/로그인 완성, 홈화면 구성 완성

--- src/pages/waffles.jsx
+++ src/pages/waffles.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Waffles() {
   return (
-    <>
+    <div>
       <h1>Waffles</h1>
-    </>
+      <p><Link to="/members/{member.id}">멤버보기</Link></p>
+      <p><Link to="/">홈으로</Link></p>
+    </div>
   );
 }
